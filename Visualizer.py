6e5cd89388910f0cbd608341428e723087299d54
Sebastian Quilter <squilter@mit.edu>
2022-05-25 13:28:07 -0700

    fix plot

--- Visualizer.py
+++ Visualizer.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
 from matplotlib.widgets import Slider
 from Dynamics import nearest
 
@@ -12,38 +11,33 @@ def plot_policy(policy):
         times.add(t)
         vels.add(vel)
         heights.add(height)
+    times = list(times)
+    times.sort()
+    vels = list(vels)
+    vels.sort()
+    heights = list(heights)
+    heights.sort()
 
-    fig = plt.figure()
+    data = np.ndarray([len(times), len(vels), len(heights)])
 
-    ax1 = fig.add_axes([0, 0, 1, 0.8], projection = '3d')
-    ax2 = fig.add_axes([0.1, 0.85, 0.8, 0.1])
+    for i, t in enumerate(times):
+        for j, v in enumerate(vels):
+            for k, h in enumerate(heights):
+                data[i, j, k] = policy[(t, v, h)]
 
-    s = Slider(ax = ax2, label = 'Burn time remaining (s)', valmin = min(times), valmax = max(times), valinit = max(times))
+    fig = plt.figure(1, figsize=(6,6))
+    main_ax = fig.add_axes([0.1,0.2,0.8,0.7])
+    slider_ax  = fig.add_axes([0.1,0.1,0.8,0.05])
+
+    main_ax.imshow(data[0,:,:], aspect='auto')
+
+    my_slider = Slider(slider_ax, 'Burn time remaining (s)', valmin = 0, valmax = len(times), valinit = 0)
 
     def update(val):
-        time = nearest(np.array(list(times)), s.val)
-        x,y = np.meshgrid(list(vels), list(heights))
-        z = np.ndarray([len(heights), len(vels)])
+        main_ax.imshow(data[int(val),:,:], aspect='auto')
+        main_ax.set_xlabel("Speed towards ground (m/s)")
+        main_ax.set_ylabel("Height (m)")
+        plt.draw()
 
-        print(x.shape)
-        print(y.shape)
-        print(z.shape)
-
-        for i in range(len(heights)): # 401
-            for j in range(len(vels)): # 101
-                v = x[i,j]
-                h = y[i,j]
-                z[i, j] = policy[(time, v, h)]
-        
-        ax1.cla()
-        ax1.plot_surface(x, y, z, cmap = cm.coolwarm, linewidth = 0, antialiased = False)
-        ax1.set_zlim(0, 100)
-
-        ax1.set_xlabel("Speed towards ground (m/s)")
-        ax1.set_ylabel("Height (m)")
-
-    s.on_changed(update)
-    update(0.0)
-
+    my_slider.on_changed(update)
     plt.show()
-
